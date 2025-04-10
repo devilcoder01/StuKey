7761b90da3623fd83868fc5e650e3629ae841475
Subhadip Jana <subhadipjana69@gmail.com>
2025-04-10 19:13:35 +0530

    feat: Update Toast component to use FontAwesome icons and improve styling

--- src/components/common/Toast.tsx
+++ src/components/common/Toast.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Toast as ToastType, useToast } from '../../context/ToastContext';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleCheck, faCircleExclamation, faExclamation, faTriangleExclamation, faXmark } from '@fortawesome/free-solid-svg-icons';
 interface ToastProps {
   toast: ToastType;
 }
@@ -13,15 +14,15 @@ const Toast: React.FC<ToastProps> = ({ toast }) => {
   const getBgColor = () => {
     switch (toast.type) {
       case 'success':
-        return 'bg-green-500';
+        return 'bg-[#78A083]';
       case 'error':
-        return 'bg-red-500';
+        return 'bg-[#BE3144]';
       case 'warning':
-        return 'bg-yellow-500';
+        return 'bg-[#D49B54]';
       case 'info':
-        return 'bg-blue-500';
+        return 'bg-[#0E2954]';
       default:
-        return 'bg-gray-700';
+        return 'bg-[#A594F9]';
     }
   };
 
@@ -30,27 +31,19 @@ const Toast: React.FC<ToastProps> = ({ toast }) => {
     switch (toast.type) {
       case 'success':
         return (
-          <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path>
-          </svg>
+          <FontAwesomeIcon icon={faCircleCheck} />
         );
       case 'error':
         return (
-          <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path>
-          </svg>
+          <FontAwesomeIcon icon = {faXmark} />
         );
       case 'warning':
         return (
-          <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd"></path>
-          </svg>
+         <FontAwesomeIcon icon={faTriangleExclamation} />
         );
       case 'info':
         return (
-          <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd"></path>
-          </svg>
+          <FontAwesomeIcon icon={faCircleExclamation} />
         );
       default:
         return null;
@@ -81,24 +74,22 @@ const Toast: React.FC<ToastProps> = ({ toast }) => {
 
   return (
     <div 
-      className={`flex items-center w-full max-w-xs p-4 mb-4 text-white rounded-lg shadow-lg ${getBgColor()} ${
+      className={`flex items-center w-full max-w-xs p-4 mb-4 text-white rounded-full shadow-lg ${getBgColor()} ${
         isExiting ? 'animate-fade-out' : 'animate-fade-in'
-      } transition-opacity duration-300`}
+      } transition-opacity duration-3000 relative`}
       role="alert"
     >
-      <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-white">
+      <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8  text-white">
         {getIcon()}
       </div>
-      <div className="ml-3 text-sm font-normal">{toast.message}</div>
+      <div className="mx-2 text-sm font-semibold">{toast.message}</div>
       <button
         type="button"
-        className="ml-auto -mx-1.5 -my-1.5 text-white hover:text-gray-200 focus:outline-none"
+        className="ml-auto -mx-1.5 -my-1.5 text-white hover:text-gray-200 focus:outline-none absolute top-4 right-4 opacity-0"
         onClick={handleClose}
         aria-label="Close"
       >
-        <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-          <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path>
-        </svg>
+        <FontAwesomeIcon icon={faXmark} />
       </button>
     </div>
   );
