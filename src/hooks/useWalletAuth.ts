24b885bb6ca11b751008ae9fc8d621f22edbdb0f
Subhadip Jana <subhadipjana69@gmail.com>
2025-04-10 18:05:38 +0530

    feat: Integrate Toast notifications for wallet connection and authentication success messages

--- src/hooks/useWalletAuth.ts
+++ src/hooks/useWalletAuth.ts
@@ -3,12 +3,14 @@ import { useWallet } from "../context/WalletContext";
 import { useSignAuth } from "../context/authSingnatureContext";
 import { signMessage } from "../utils/signmessage";
 import { EIP6963ProviderDetail } from "../types/wallet.types";
+import { useToastNotification } from "./useToastNotification";
 
 export const useWalletAuth = () => {
   const { connectWallet, disconnectWallet, selectedWallet, userAccount } =
     useWallet();
   const { login, logout } = useSignAuth();
   const [error, setError] = useState<string | null>(null);
+  const { showSuccess, showError, showInfo } = useToastNotification();
 
   // Helper to handle errors
   const handleError = (message: string) => {
@@ -19,6 +21,7 @@ export const useWalletAuth = () => {
   const walletConnect = async (provider: EIP6963ProviderDetail) => {
     try {
       await connectWallet(provider);
+      showSuccess("Wallet connected successfully!");
       return true;
     } catch (err) {
       setError(err instanceof Error ? err.message : "Failed to connect wallet");
@@ -26,27 +29,25 @@ export const useWalletAuth = () => {
   };
 
   const signinMessage = async () => {
-    try
-    {
-
+    try {
       if (!selectedWallet || !userAccount) {
         return handleError("Failed to connect wallet");
       }
-      
+
       // 2. Sign Message
       const message = "Sign this message to verify ownership of this wallet.";
       const signature = await signMessage(selectedWallet, userAccount, message);
-      
+
       if (typeof signature !== "string") {
         return handleError("Failed to sign message");
       }
-      
+
       // 3. Login (auth backend)
       await login(userAccount, signature);
-      
+      showSuccess("Authenticated successfully!");
+
       return true;
-    }
-    catch (err) {
+    } catch (err) {
       return handleError(
         err instanceof Error ? err.message : "Fail to sign the message"
       );
@@ -102,6 +103,6 @@ export const useWalletAuth = () => {
     error,
     clearError,
     walletConnect,
-    signinMessage
+    signinMessage,
   };
 };
